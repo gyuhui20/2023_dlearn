4110232028b893099ba27424b626a6cfd6f1a619
gyuhui20 <https://github.com/gyuhui20/2023_dlearn.git>
2024-01-02 15:35:40 +0900

    fashion_mn classification with DNN

---
+++ fashion_mnist_graph.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import pandas as pd
+import tqdm as tqdm
+
+#test graph
+print("number of correct answer", acc_test, "/", len(xs_test))
+
+plt.figure(figsize=(5,5))
+sample_size = 9
+random_idx = np.random.randint(100, size=sample_size)
+for i, idx in enumerate(random_idx):
+    plt.subplot(3,3,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(xs_test[idx].reshape(28,28), cmap='gray')
+    plt.xlabel(f"real:{np.argmax(ys_test[idx])} : predict:{np.argmax(y_test_pred[idx])}")
+plt.show()
